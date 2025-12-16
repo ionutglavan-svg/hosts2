21e9e85b0948ef595d9d8f78c0a3544c3ab060cf
ionutglavan-svg <ionut.glavan@cnodobescu.ro>
2025-12-16 15:54:51 +0000

    Cel de al doilea commit

---
+++ verIP2.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+ k=0
+nslookup $1 $3  | while read restul ip2 
+do
+
+    if [ "$restul" = "Address:" ]
+    then
+        k=$((k+1))
+        if [ "$k" -eq 2  -a "$2" != "$ip2"  ]
+        then
+            echo "Bogus IP for $1 in /etc/hosts! *\n" 
+                
+        fi
+    fi
+       
+    done 
+   
+    
+
+exit 0
